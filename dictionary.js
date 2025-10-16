8ca22d05d47de9c827db32d2836a8cf40a289526
idontknow95959-jpg <idontknow95959@gmail.com>
2025-10-17 01:12:10 +0900

    dictionary개선

--- dictionary.js
+++ dictionary.js
@@ -9,14 +9,28 @@
 // - 단어 경계를 인식하므로 "rob"이 "robin"에 매칭되지 않습니다
 // - 고유명사, 캐릭터 이름, 특수 용어 등에 사용하세요
 
-const localDictionary = new Map([
+const localDictionary = {
     // ===== 주요 관계 =====
-    ["robin", "로빈"],
-    ["whitney", "휘트니"],
+    "robin": "로빈",
+    "whitney": "휘트니",
 
     // ===== 관심 인물 =====
-    ["bailey", "베일리"],
-    ["briar", "브라이어"],
-    ["quinn", "퀸"],
-    ["remy", "레미"],
-]);
+    "bailey": "베일리",
+    "briar": "브라이어",
+    "charlie": "찰리",
+    "darryl": "데릴",
+    "doren": "도렌",
+    "gwylan": "그윌란",
+    "harper": "하퍼",
+    "jordan": "조던",
+    "landry": "랜드리",
+    "leighton": "레이턴",
+    "mason": "메이슨",
+    "morgan": "모건",
+    "river": "리버",
+    "sam": "샘",
+    "sirris": "시리스",
+    "winter": "윈터",
+    "quinn": "퀸",
+    "remy": "레미",
+};
